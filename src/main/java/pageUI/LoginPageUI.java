fa0d7dea3b464ee69a7f5daa88bf3bd874b91cf0
phandinh2010 <phandinh2010@gmail.com>
2021-12-01 23:02:18 +0700

    Login page UI

---
+++ src/main/java/pageUI/LoginPageUI.java
@@ -0,0 +1,11 @@
+package pageUI;
+
+public class LoginPageUI {
+	public static final String EMAIL_TEXTBOX= "//input[@id='Email']";
+	public static final String EMAIL_EMPTY_MSG= "//span[@id='Email-error']";
+	public static final String EMAIL_INVALID_MSG= "//span[@class='field-validation-error']";
+	public static final String EMAIL_NOT_EXIST_MSG= "//div[@class='message-error validation-summary-errors']";
+	public static final String PASSWORD_TEXTBOX= "//input[@id='Password']";
+	public static final String PASSWORD_EMPTY_MSG= "//div[@class='message-error validation-summary-errors']";
+	public static final String PASSWORD_WRONG_MSG= "//div[@class='message-error validation-summary-errors']";
+}
